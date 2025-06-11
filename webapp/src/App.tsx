6e8459327153e453cdbeb16912fb9bda334a11c2
QuatraQ <rn8530638@gmail.com>
2025-06-11 16:34:43 +0300

    Leson 35

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -6,19 +6,19 @@ import { ViewIdeaPage } from './pages/ViewIdeaPage'
 import { Layout } from './componets/layout'
 import './styles/global.scss'
 import { NewIdeaPage } from './pages/NewIdeaPage'
-    
+
 export const App = () => {
   return (
     <TrpcProvider>
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
-          <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />} />
-          <Route path={routes.getAllIdeasRoute()} element={<NewIdeaPage />} />
-          <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+            <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />} />
+            <Route path={routes.getNewIdeaRoute()} element={<NewIdeaPage />} />
+            <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
-    </TrpcProvider> 
+    </TrpcProvider>
   )
 }
--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,10 +1,58 @@
-import css from './index.module.scss'   
+import { Segment } from '../../componets/segment'
+import { Input } from '../../componets/input'
+import { Textarea } from '../../componets/Textarea'
+import { useFormik } from 'formik'
 
 export const NewIdeaPage = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      nick: '',
+      description: '',
+      text: '',
+    },
+    validate: (values) => {
+      const errors: Partial<typeof values> = {}
+      if (!values.name) {
+        errors.name = 'Name is required'
+      }
+      if (!values.nick) {
+        errors.nick = 'Nick is required'
+      } else if (!values.nick.match(/^[a-z0-9-]+$/)) {
+        errors.nick = 'Nick may contain only lowercase letters, numbers and dashes'
+      }
+      if (!values.description) {
+        errors.description = 'Description is required'
+      }
+      if (!values.text) {
+        errors.text = 'Text is required'
+      } else if (values.text.length < 50) {
+        errors.text = 'Text should be at least 50 characters long'
+      }
+      return errors
+    },
+    onSubmit: (values) => {
+      console.info('Submitted', values)
+    },
+  })
+
+  console.log(formik)
+
   return (
-    <div>
-      <h1 className={css.title}>New Idea</h1>
-      <div>Form will be here...</div>
-    </div>
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          formik.handleSubmit()
+        }}
+      >
+        <Input name="name" label="Name" formik={formik} />
+        <Input name="nick" label="Nick" formik={formik} />
+        <Input name="description" label="Description" formik={formik} />
+        <Textarea name="text" label="Text" formik={formik} />
+        {!formik.isValid && !!formik.submitCount &&<div style={{ color: 'red' }}>Some fields are invalid</div>}
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
   )
 }
